966a5fd88075fb33daa9981915fc9e85ccc087cf
edidudedo <eduardoiglesius101@gmail.com>
2024-09-08 01:46:30 +0900

    Add State Transition

--- IVRC_Unity2/Assets/Scripts/Tools/StateManager.cs
+++ IVRC_Unity2/Assets/Scripts/Tools/StateManager.cs
@@ -4,12 +4,17 @@ public class StateManager : MonoBehaviour
 {
     public string stateNumber; 
     private string previousStateNumber;
+    GameObject vrWorldObject;
+    public GameObject cheese;
+    public GameObject wine;
+    public GameObject baguette;
 
     public TextManager textManager;
 
     void Start()
     {
         textManager = FindObjectOfType<TextManager>();
+        vrWorldObject = GameObject.Find("World/vrWorld");
 
         previousStateNumber = stateNumber;
 
@@ -21,6 +26,7 @@ public class StateManager : MonoBehaviour
 
     void Update()
     {
+        Debug.Log(vrWorldObject.layer);
         if (stateNumber != previousStateNumber)
         {
             if (textManager != null)
@@ -29,6 +35,40 @@ public class StateManager : MonoBehaviour
             }
 
             previousStateNumber = stateNumber;
+        }
+        ChangeStateNumber();
+    }
+
+    void ChangeStateNumber()
+    {
+        if(stateNumber == "1")
+        {
+            if(vrWorldObject.layer == 0)
+            {
+                stateNumber = "2";
+            }
+        }
+        else if(stateNumber == "2")
+        {
+            if (cheese.GetComponent<ObjectManager>().isIdle || baguette.GetComponent<ObjectManager>().isIdle || wine.GetComponent<WineManager>().isIdle)
+            {
+                stateNumber = "3";
+            }
+        }
+        else if (stateNumber == "3")
+        {
+            //FIXME
+        }
+        else if (stateNumber == "4")
+        {
+            //FIXME
+        }
+        else if (stateNumber == "5")
+        {
+            if (vrWorldObject.layer == 6)
+            {
+                stateNumber = "6";
+            }
         }
     }
 }
