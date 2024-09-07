65a06212fe00a9be752cb66055b9c046f2969287
Junshu <44729013+HoganAn@users.noreply.github.com>
2024-09-08 01:24:41 +0900

    Painting texture adapts to state variations

--- IVRC_Unity2/Assets/Portal/Scripts/Dissolution.cs
+++ IVRC_Unity2/Assets/Portal/Scripts/Dissolution.cs
@@ -8,11 +8,54 @@ public class Dissolution : MonoBehaviour
     public Transform headTransform;
     public Transform rightHandTransform;
     public Transform leftHandTransform;
+    public Texture2D initialPainting;
+    public Texture2D finishedPainting;
+    private StateManager stateManager;
+
+    private void Start()
+    {
+        GameObject stateManagerObject = GameObject.Find("StateManager");
+
+        if (stateManagerObject != null)
+        {
+            // Get the StateManager component
+            stateManager = stateManagerObject.GetComponent<StateManager>();
+
+            // Check if the stateManager and lightAnimation components are found
+            if (stateManager != null)
+            {
+                // Check the stateNumber and enable/disable lightAnimation accordingly
+                UpdatePaintingTexture();
+            }
+            else
+            {
+                Debug.LogError("StateManager component not found!");
+            }
+        }
+        else
+        {
+            Debug.LogError("StateManager GameObject not found!");
+        }
+    }
 
     void Update()
     {
         mat.SetVector("_HeadPos", headTransform.position);
         mat.SetVector("_RightHandPos", rightHandTransform.position);
         mat.SetVector("_LeftHandPos", leftHandTransform.position);
+
+        UpdatePaintingTexture();
+    }
+
+    void UpdatePaintingTexture()
+    {
+        if (stateManager.stateNumber == "6")
+        {
+            mat.SetTexture("_Painting", finishedPainting);
+        }
+        else
+        {
+            mat.SetTexture("_Painting", initialPainting);
+        }
     }
 }
