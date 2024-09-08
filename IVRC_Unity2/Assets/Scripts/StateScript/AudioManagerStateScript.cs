e1dcbfdf9d1a87e1c200a0e0e401d559f5fa0c2d
edidudedo <eduardoiglesius101@gmail.com>
2024-09-08 15:57:48 +0900

    Font Bug Fix, BGM control, Voice delay

---
+++ IVRC_Unity2/Assets/Scripts/StateScript/AudioManagerStateScript.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManagerStateScript : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    // Reference to the StateManager
+    private StateManager stateManager;
+
+    void Start()
+    {
+        // Find the StateManager object
+        GameObject stateManagerObject = GameObject.Find("StateManager");
+
+        if (stateManagerObject != null)
+        {
+            // Get the StateManager component
+            stateManager = stateManagerObject.GetComponent<StateManager>();
+
+            // Get the LightAnimation script from this GameObject (assuming it's attached to the same object)
+            audioSource = GetComponent<AudioSource>();
+
+            // Check if the stateManager and lightAnimation components are found
+            if (stateManager != null && audioSource != null)
+            {
+                // Check the stateNumber and enable/disable lightAnimation accordingly
+                UpdateSoundAnimationState();
+            }
+            else
+            {
+                Debug.LogError("StateManager or LightAnimation component not found!");
+            }
+        }
+        else
+        {
+            Debug.LogError("StateManager GameObject not found!");
+        }
+    }
+
+    void Update()
+    {
+        UpdateSoundAnimationState();
+    }
+
+    void UpdateSoundAnimationState()
+    {
+        Debug.Log("StateNumber : " + stateManager.stateNumber);
+        if (stateManager.stateNumber == "2" || stateManager.stateNumber == "3" || stateManager.stateNumber == "4" || stateManager.stateNumber == "5")
+        {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else if (stateManager.stateNumber == "1" || stateManager.stateNumber == "6")
+        {
+            audioSource.Stop();
+        }
+    }
+
+
+}
